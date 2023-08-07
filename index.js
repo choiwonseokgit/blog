aa5f0f7e97c5b456cb14492052d6b8bbbdd73e25
choiwonseokgit <cs0328@hanmail.net>
2023-08-07 20:08:08 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://cs0328:a12345@cluster0.4gfyvr1.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("mongoDB connected..!!"))
   .catch((err) => console.log(err));
 
@@ -19,7 +26,23 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Hello World! 최원석의 blog 서버이다.");
 });
-//cs0328, a12345
+
+app.post("/register", (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client 에서 가져오면
+  //그것들을 DB에 넣어준다.
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      return res.json({ success: false, err });
+    });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
